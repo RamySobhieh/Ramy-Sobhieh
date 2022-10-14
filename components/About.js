57d7a15f12116d78da2b0aec46df8280f548776a
RamySobhieh <ramysobhieh@email.com>
2022-10-14 18:39:53 +0200

    Change in the about section to fit the image better

--- components/About.js
+++ components/About.js
@@ -26,8 +26,8 @@ function About() {
 
   return (
   <div id='about' className=' w-full md:h-screen p-6 flex items-center py-16 snap-start'>
-    <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8 text-center md:text-start'>
-      <div  className='col-span-2 mb-10'>
+    <div className='max-w-[1240px] m-auto md:grid grid-cols-2 xl:grid-cols-3 gap-8 text-center md:text-start'>
+      <div  className='col-span-1 xl:col-span-2 mb-10'>
         <p ref={ref} className='uppercase text-xl font-bold tracking-widest text-[#8105D8]'>
           About
         </p>
@@ -57,7 +57,7 @@ function About() {
           <button type="button" className="rounded-lg text-[#8105D8] hover:text-white/[0.87] border border-[#8105D8] hover:bg-[#8105D8] focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium text-m px-5 py-2.5 text-center mr-2 mb-2 ease-in duration-150 h-[60px] w-[200px] uppercase">Download Resume</button>
         </a>
       </div>
-      <div className='w-[60%] md:w-[80%] h-auto m-auto flex items-center justify-center rounded-lg'>
+      <div className='w-[60%] md:w-[80%] min-w-[300px] h-auto m-auto flex items-center justify-center rounded-lg'>
         <div className="relative group">
           <div className="absolute -inset-1 bg-gradient-to-b from-[#8105D8] to-[#FE59D7] blur opacity-75 group-hover:opacity-100  duration-1000 group-hover:duration-300 ease-in-out"></div>
           <Image src={AboutImg} className='w-full h-full object-cover rounded-lg' alt='/' />
